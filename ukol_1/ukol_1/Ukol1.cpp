10ebda1110ad8e2da19c712f4325e735b3cc51d5
JKalina08 <kalina08@student.vspj.cz>
2022-11-06 22:22:14 +0100

    boiler ukol 1

---
+++ ukol_1/ukol_1/Ukol1.cpp
@@ -0,0 +1,56 @@
+// Ukol1.cpp : Tento soubor obsahuje funkci main. Provádìní programu se tam zahajuje a ukonèuje.
+//
+
+#include <iostream>
+using namespace std;
+
+// ----------------------------------------------------------------
+// Domaci ukol:
+
+class CislaOperace
+{
+	int faktorialNerek(int n)
+	{
+		int x = 1;
+		for (int i = 1; i <= n; i++) {
+			x *= i;
+
+		}
+		return x;
+	}
+
+	int mocninaR(int c, int m)
+	{
+		// todo du
+		return -1;
+	}
+
+	int mocninaNerek(int c, int m)
+	{
+		// todo du
+		return -1;
+	}
+
+	int zjistiPocetKladnych(int* hodnoty, int n)
+	{
+		// todo du
+		return -1;
+	}
+};
+
+int main()
+{
+	z =
+	cout << CislaOperace.faktorialNerek(4) << endl;
+}
+
+// Spuštìní programu: Ctrl+F5 nebo nabídka Ladit > Spustit bez ladìní
+// Ladìní programu: F5 nebo nabídka Ladit > Spustit ladìní
+
+// Tipy pro zahájení práce:
+//   1. K pøidání nebo správì souborù použijte okno Prùzkumník øešení.
+//   2. Pro pøipojení ke správì zdrojového kódu použijte okno Team Explorer.
+//   3. K zobrazení výstupu sestavení a dalších zpráv použijte okno Výstup.
+//   4. K zobrazení chyb použijte okno Seznam chyb.
+//   5. Pokud chcete vytvoøit nové soubory kódu, pøejdìte na Projekt > Pøidat novou položku. Pokud chcete pøidat do projektu existující soubory kódu, pøejdìte na Projekt > Pøidat existující položku.
+//   6. Pokud budete chtít v budoucnu znovu otevøít tento projekt, pøejdìte na Soubor > Otevøít > Projekt a vyberte pøíslušný soubor .sln.
