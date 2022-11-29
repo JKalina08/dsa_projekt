5a5165f38f692222af9d0c2c3a817fd07684cd96
JKarmasin <karmasin@student.vspj.cz>
2022-11-29 21:34:57 +0100

    ukol 4

--- ukol_4/ukol_4/LinekdList.cpp
+++ ukol_4/ukol_4/LinekdList.cpp
@@ -179,8 +179,13 @@ bool MyList::Test_End(ListNode* pNode)
 
 int MyList::Pocet_Prvku()
 {
-	// todo1 du
-	return -1;
+	int i = 0;
+	ListNode* pTmp = pHead;
+	while (pTmp != NULL) {
+		i++;
+		pTmp = pTmp->pNext;
+	}
+	return i;
 }
 
 // --
@@ -208,14 +213,27 @@ void MyList::TiskReverse()
 
 int MyList::SumaCisel()
 {
-	// todo2 du
-	return 0;
+	int i = 0;
+	ListNode* pTmp = pHead;
+	while (pTmp != NULL) {
+		i += pTmp->value;
+		pTmp = pTmp->pNext;
+	}
+	return i;
 }
 
 int MyList::SumaCisel_BezKrajnichHodnot()
 {
-	// todo3 du
-	return 0;
+	int i = 0;
+	ListNode* pTmp = pHead;
+	pTmp = pTmp->pNext; // preskoceni prvniho
+
+	while (pTmp != NULL) {
+
+		if(pTmp->pNext != NULL) i += pTmp->value;
+		pTmp = pTmp->pNext;
+	}
+	return i;
 }
 
 int MyList::PocetRuznych()
@@ -235,10 +253,119 @@ void MyList::AddSudeMultiple()
 
 void MyList::MoveMinHead()
 {
-	// todo4 du
+	ListNode* pTmp = pHead; // temp Pointer na zacatek SpojSeznamu
+	ListNode* min = pHead;
+
+	while (pTmp != NULL) {
+		if (min->value > pTmp->value) {
+			min = pTmp;
+		}
+		pTmp = pTmp->pNext;
+	}
+
+	if (min == pHead)
+		return;
+
+	ListNode* pre_min = pHead; // Pointer
+	pTmp = pHead->pNext;
+
+	while (pTmp != NULL) {
+		if (pre_min->pNext->value > pTmp->value) { // test: hodnota  následujícího prvku za pre_min není nejvìtší a pak posunem ukazujem na predchazejici prvek prvku minimalnimu
+			pre_min = pTmp;
+		}				
+		pTmp = pTmp->pNext;
+	}
+
+	pTmp = pre_min->pNext;
+	
+	pre_min->pNext = pre_min->pNext->pNext;
+	
+	pTmp->pNext = pHead;
+	pHead = pTmp;
 }
 
 void MyList::MoveMaxTail()
 {
 	// todo5 du
+	ListNode* pTmp = pHead; // temp Pointer na zacatek SpojSeznamu
+	ListNode* max = pHead;
+
+	// projdeme cele pole a hledame max. hodotu
+	while (pTmp != NULL) {
+		if (max->value < pTmp->value) {
+			max = pTmp;
+		}
+		pTmp = pTmp->pNext;
+	}
+
+
+	//std::cout << max->value << '\n';
+
+	// 2   5   6   1   8   4
+	ListNode* pTail = pHead;
+	//
+	for (int i = 0; i < this->Pocet_Prvku() - 1; i++) {
+		pTail = pTail->pNext;
+	}
+
+	//std::cout << pTail->value << '\n';
+
+	if (pTail->value == max->value) {
+			return;
+	}
+
+	if (max == pHead) {
+		pHead = pHead->pNext;
+		pTail->pNext = max;
+		max->pNext = NULL;
+		return;
+	}
+
+	ListNode* pre_max = pHead;
+
+	while (pre_max != NULL) {
+		if (pre_max->pNext->value == max->value) { // test: hodnota  následujícího prvku za pre_min není nejvìtší a pak posunem ukazujem na predchazejici prvek prvku minimalnimu
+			pTail->pNext = max;
+			pre_max->pNext = pre_max->pNext->pNext;
+			max->pNext = NULL;
+			return;
+		}
+		pre_max = pre_max->pNext;
+	}
+}
+
+int main() 
+{
+	MyList* ml = new MyList();
+	for (int i=10; i>2; i--){
+		ml->Add(i);
+	}
+	ml->Add(7);
+
+
+	ml->Tisk();
+	std::cout << std::endl;
+
+	int pp = ml->Pocet_Prvku();
+	std::cout << "Pocet prvku: " << pp << '\n';
+	std::cout << std::endl;
+
+	pp = ml->SumaCisel();
+	std::cout << "Suma cisel: " << pp << '\n';
+	std::cout << std::endl;
+
+	pp = ml->SumaCisel_BezKrajnichHodnot();
+	std::cout << "Suma cisel bez krajnich hodnot: " << pp << '\n';
+	std::cout << std::endl;
+
+	ml->MoveMinHead();
+	ml->Tisk();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	ml->MoveMaxTail();
+	ml->Tisk();
+	std::cout << std::endl;
+	return 0;
+
 }
